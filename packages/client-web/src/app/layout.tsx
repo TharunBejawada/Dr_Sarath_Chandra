434498c147cceaf60d47d8a2bb0f2cc01f673883
Tharun Bejawada <tharunbejawada@Tharuns-MacBook-Air.local>
2026-07-04 19:27:58 +0530

    GTM updated

--- packages/client-web/src/app/layout.tsx
+++ packages/client-web/src/app/layout.tsx
@@ -24,13 +24,15 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <CanonicalTag />
+        
+        {/* Updated Google Tag Manager */}
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NKRLX977');
+            })(window,document,'script','dataLayer','GTM-PK3Z7GMK');
           `}
         </Script>
 
@@ -48,14 +50,17 @@ export default function RootLayout({
         </Script>
       </head>
       <body className={`${inter.variable} ${playfair.variable} font-sans bg-white flex flex-col min-h-screen`}>
+        
+        {/* Updated Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NKRLX977"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PK3Z7GMK"
             height="0" 
             width="0" 
             style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
+        
         <Navbar />
         <main className="flex-grow">{children}</main>
         <Footer /> 
